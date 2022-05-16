36e13f3e6f3c112e5c1eac50e0e3a08b06e29039
Alejandroe0 <alejandroecfm@gmail.com>
2022-05-16 11:00:27 -0600

    Se avanzo con las funciones

--- EXAMENFINAL/Examen_Final.c
+++ EXAMENFINAL/Examen_Final.c
@@ -1,8 +1,8 @@
 /*
 Autor:         Alejandro Barillas, inspirado por Maynor Ballina
-Fecha:         Tue Apr 26 23:03:04 CST 2022
+Fecha:         Mon May 16 08:28:07 CST 2022
 compilador:    gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
-Compilar:      gcc -o Newton_Raphson.out Newton_Raphson.c -lm
+Compilar:      gcc -o Examen_Final.out Examen_Final.c -lm
 Librerias:     stdio, stdlib, math
 Resumen:       Ejemplo basico del metodo numerico de NewtonRaphson para 2+cos(e^(x)-2)-e^(x)
 */
@@ -13,41 +13,46 @@ Resumen:       Ejemplo basico del metodo numerico de NewtonRaphson para 2+cos(e^
 #include <math.h>
 
 //prototipos de funciones
-// Funcion original
-float f(float x);
+// Funcion para la simulacion
+float cohete1(float E0, float TSFC, float CD, float A, float m0, float mf0);
 // La derivada de la funcion
 float df(float x);
+
+//La funcion de Fa
+float fa(float x);
+// La derivada de la funcion de Fa
+float dfa(float x);
 //El método Númerico
 void NewtonRaphson(float x0, float tol, int maxiter, int *actiter, float *sol);
+
 
 void main (void)
 {
     //definir variables
-    float x_inicial, tolerancia, xS;
+    float G, Rt, Mt, R, L, g0, T0, P0;
     int iteraciones, Aiteracion;
     
+    //Declaro mis constantes
+    G=6.693*pow(10,-11);
+    Rt=6.378*pow(10,6);
+    Mt=5.9736*pow(10,24);
+    R=287.06;
+    L=6.5*pow(10,-3);
+    g0=9.81;
+    T0=288.15;
+    P0=101325;
+
     //obtener datos
     printf("Ingrese el valor aproximado de x: ");
     scanf("%f",&x_inicial);
     printf("Ingrese el valor de tolerancia: ");
     scanf("%f",&tolerancia);
-    printf("Ingrese el valor maximo de iteraciones: ");
+    printf("Ingrese el valor maximo de iteraciones: (se recomienda 100)");
     scanf("%d",&iteraciones);
 
-    //Ejecuta el metodo númerico
-    NewtonRaphson(x_inicial, tolerancia, iteraciones, &Aiteracion, &xS);
-    //Verifica si ya se cumplio el numero de iteraciones
-    if (Aiteracion == iteraciones)
-        //Si si se cumplio y no se ha llegado a la tolerancia devuelve el mensaje
-        printf("\nNo hay solucion despues de %d iteraciones\n",iteraciones);
-    else
-    {
-        //Si ya llego a la tolerancia, muestra el resultado y el numero de iteraciones
-        printf("\nLuego de %d iteraciones la solucion es %.4f\n",Aiteracion,xS);
-    }
 }
 
-//Declaracion del método numerico
+
 void NewtonRaphson(float x0, float tol, int maxiter, int *actiter, float *sol)
 {
     // Declaro la variables
@@ -75,20 +80,20 @@ void NewtonRaphson(float x0, float tol, int maxiter, int *actiter, float *sol)
     *actiter = i;
 }
 
-// Se declara la funcion original
-float f(float x)
+// Se declara la funcion fa
+float fa(float x, float CD, float A, float P)
 {
     
     float res = 0, aux1=0;
     //Es mi variable del argumento del coseno
-    aux1 = exp(x)-2;
+    aux1 = (1/2)*P;
     //La variable almacena La funcion original
     res = 2 + cos(aux1)-exp(x);
     return res;
 }
 
-// Se declara la primera derivada de la funcion original
-float df(float x)
+// Se declara la primera derivada de la funcion fa
+float dfa(float x, float CD, float A, float P)
 {
     //Declarto las variables locales
     float res = 0, aux1=0;
