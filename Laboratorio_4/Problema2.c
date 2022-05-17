ce78a41d10d6ffa5fd5c4787109f6aa85afcc8a2
Alejandroe0 <alejandroecfm@gmail.com>
2022-05-16 18:37:53 -0600

    Se termino el problema 2

--- Laboratorio_4/Problema2.c
+++ Laboratorio_4/Problema2.c
@@ -13,3 +13,50 @@ Resumen:       Se ordenara una lista de 10 numeros pares
 #include <stdlib.h>
 #include <math.h>
 
+//Declaro la variable del vector 
+int data[5];
+
+
+int main(){
+    //1. For para almacenar los numeros del usuario
+    for (int i = 0; i < 5; i++)
+    {
+        //Pido la posicion i del vector
+        printf("ingresar la posicion %d del vector\n",(i+1));
+        scanf("%d", &data[i]);
+    }
+    
+    //Inicio el ordenamiento
+    puts("\nOrdenando");
+        int interruptor = 1;
+        int pasada, j;
+        //bucle que controla la cantidad de pasadas
+        for (pasada = 0; pasada < 5-1 && interruptor; pasada++)
+        {
+            interruptor = 0;
+            //maneja el paso por el vector
+            for (j = 0; j < 5-pasada-1; j++)
+            {
+                //validando que el seleccionado sea mayor al siguiente
+                if (data[j] > data[j+1])
+                {
+                    int aux;
+                    printf("cambio %d %d a ",data[j],data[j+1]);
+                    aux = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = aux;
+                    printf("%d %d \n",data[j],data[j+1]);
+                }
+                interruptor = 1;
+            }   
+        }
+        puts("\nVertor Ordenado");
+        for (int i = 0; i < 5; i++)
+        {
+            /* Imprimo el vector ordenado  */
+            printf("%d ",data[i]);
+        }
+        puts("\nFIN");
+    
+    return 0;
+}
