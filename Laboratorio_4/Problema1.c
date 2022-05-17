4df1e9e8a8dc3cb4352e2a3755757536fe8bdd3e
Alejandroe0 <alejandroecfm@gmail.com>
2022-05-16 18:19:08 -0600

    problema 1 terminado solo numerico

--- Laboratorio_4/Problema1.c
+++ Laboratorio_4/Problema1.c
@@ -9,6 +9,7 @@ Resumen:       Se ordenara una lista de 10 numeros pares
 
 //librerias
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <math.h>
 
@@ -17,37 +18,94 @@ void main (void)
 {
     //definir variables
     int data[10]={2,20,4,18,6,16,8,14,10,12};
-   
-    puts("\nOrdenando");
-    int interruptor = 1;
-    int pasada, j;
-    //bucle que controla la cantidad de padas
-    for (pasada = 0; pasada < 9 && interruptor; pasada++)
+    int val=0, val1;
+    
+
+    while (val==0)
     {
-        interruptor = 0;
-        //maneja el paso por el vector
-        for (j = 0; j < 10-pasada-1; j++)
+        printf("si lo quiere ordenar de forma asendente ingrese 1\n");
+        printf("si lo quiere ordenar de forma desendente ingrese 2\n");
+        scanf("%d", &val1);
+
+        if (val1 == 1)
         {
-            //validando que el seleccionado sea mayor al siguiente
-            if (data[j] < data[j+1])
+            puts("\nOrdenando");
+            int interruptor = 1;
+            int pasada, j;
+            //bucle que controla la cantidad de padas
+            for (pasada = 0; pasada < 9 && interruptor; pasada++)
             {
-                int aux;
-                printf("cambio %d %d a ",data[j],data[j+1]);
-                aux = data[j];
-                data[j] = data[j+1];
-                data[j+1] = aux;
-                printf("%d %d \n",data[j],data[j+1]);
+                interruptor = 0;
+                //maneja el paso por el vector
+                for (j = 0; j < 10-pasada-1; j++)
+                {
+                    //validando que el seleccionado sea mayor al siguiente
+                    if (data[j] > data[j+1])
+                    {
+                        int aux;
+                        printf("cambio %d %d a ",data[j],data[j+1]);
+                        aux = data[j];
+                        data[j] = data[j+1];
+                        data[j+1] = aux;
+                        printf("%d %d \n",data[j],data[j+1]);
+                    }
+                    interruptor = 1;
+                }   
             }
-            interruptor = 1;
-        }   
+            puts("\nVertor Ordenado");
+            for (int i = 0; i < 10; i++)
+            {
+                /* ingresar numeros al azar con la funcion rand */
+                printf("%d ",data[i]);
+            }
+            puts("\nFIN");
+            val=1;
+
+
+        }else if (val1==2)
+        {
+            puts("\nOrdenando");
+            int interruptor = 1;
+            int pasada, j;
+            //bucle que controla la cantidad de padas
+            for (pasada = 0; pasada < 9 && interruptor; pasada++)
+            {
+                interruptor = 0;
+                //maneja el paso por el vector
+                for (j = 0; j < 10-pasada-1; j++)
+                {
+                    //validando que el seleccionado sea mayor al siguiente
+                    if (data[j] < data[j+1])
+                    {
+                        int aux;
+                        printf("cambio %d %d a ",data[j],data[j+1]);
+                        aux = data[j];
+                        data[j] = data[j+1];
+                        data[j+1] = aux;
+                        printf("%d %d \n",data[j],data[j+1]);
+                    }
+                    interruptor = 1;
+                }   
+            }
+            puts("\nVertor Ordenado");
+            for (int i = 0; i < 10; i++)
+            {
+                /* ingresar numeros al azar con la funcion rand */
+                printf("%d ",data[i]);
+            }
+            puts("\nFIN");
+            val=1;
+
+        }else
+        {
+            puts("Por favor ingrese una de los caracteres validos\n");
+            val=0;
+        }
+        
+        
+        
     }
-    puts("\nVertor Ordenado");
-    for (int i = 0; i < 10; i++)
-    {
-        /* ingresar numeros al azar con la funcion rand */
-        printf("%d ",data[i]);
-    }
-    puts("\nFIN");
+    
 }
 
 
