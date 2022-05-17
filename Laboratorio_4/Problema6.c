b572885d52f80b76be73011a2b00f1ea5f3a5ded
Alejandroe0 <alejandroecfm@gmail.com>
2022-05-16 23:39:26 -0600

    Se corrigio el problema 5

---
+++ Laboratorio_4/Problema6.c
@@ -0,0 +1,13 @@
+/*
+Autor:         Alejandro Barillas, inspirado por Maynor Ballina
+Fecha:         Mon May 16 23:38:14 CST 2022
+compilador:    gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
+Compilar:      gcc -o Problema5.out Problema5.c -lm
+Librerias:     stdio, stdlib, math
+Resumen:       Factorial de un numero con recursividad
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
