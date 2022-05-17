965d2b248716d01e0c18587f8ea70d7e8d30016c
Alejandroe0 <alejandroecfm@gmail.com>
2022-05-16 18:21:58 -0600

    inicio problemna 2

---
+++ Laboratorio_4/Problema2.c
@@ -0,0 +1,15 @@
+/*
+Autor:         Alejandro Barillas, inspirado por Maynor Ballina
+Fecha:         Mon May 16 18:20:04 CST 2022
+compilador:    gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0
+Compilar:      gcc -o Problema2.out Problema2.c -lm
+Librerias:     stdio, stdlib, math
+Resumen:       Se ordenara una lista de 10 numeros pares
+*/
+
+//librerias
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
